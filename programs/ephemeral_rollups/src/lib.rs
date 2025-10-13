5f62e849c5cba309620ecea98497fa139bc74b3b
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-10-13 09:51:16 +0530

    feat (anchor, major): add functions for opening, updating and closing trade positions with correct balance logic
    - Updated `TradingAccountForArena` struct to include `authority`, `open_positions_count`, and `usdc_balance`.
    - Added `admin_fn` prefix in admin functions for clarity.
    - Added error handling for insufficient funds and unsupported shorting.
    - Created new account structures for managing open positions.
    - Added TODOs for future tasks regarding price feeds and account management.

--- programs/ephemeral_rollups/src/lib.rs
+++ programs/ephemeral_rollups/src/lib.rs
@@ -5,9 +5,10 @@ declare_id!("BoKzb5RyCGLM5VuEThDesURM5hi3TRfVF84kYoiokrop");
 #[program]
 pub mod ephemeral_rollups {
     use super::*;
+    // admin_fn -> admin functions
 
     // only needed to create arenas
-    pub fn create_profile(ctx: Context<CreateUserProfile>, name: String) -> Result<()> {
+    pub fn admin_fn_create_profile(ctx: Context<CreateUserProfile>, name: String) -> Result<()> {
         require!(name.len() <= 10, EphemeralRollupError::NameTooLong);
 
         let profile_account = &mut ctx.accounts.profile_account;
@@ -19,7 +20,7 @@ pub mod ephemeral_rollups {
         Ok(())
     }
 
-    pub fn create_arena(ctx: Context<CreateArena>) -> Result<()> {
+    pub fn admin_fn_create_arena(ctx: Context<CreateArena>) -> Result<()> {
         // TODO: add checks, not everyone should be able to create an arena
 
         let arena_account = &mut ctx.accounts.arena_account;
@@ -32,21 +33,73 @@ pub mod ephemeral_rollups {
     }
 
     pub fn create_trading_account_for_arena(ctx: Context<CreateTradingAccountForArena>) -> Result<()> {
-        let trading_account_for_arena = &mut ctx.accounts.trading_account_for_arena;
+        let trading_account = &mut ctx.accounts.trading_account_for_arena;
 
-        trading_account_for_arena.bump = ctx.bumps.trading_account_for_arena;
-        trading_account_for_arena.pubkey = *ctx.accounts.signer.key;
+        trading_account.bump = ctx.bumps.trading_account_for_arena;
+        trading_account.authority = *ctx.accounts.signer.key;
+        trading_account.open_positions_count = 0;
+
+        // TODO: figure out what the starting balance should be
+        trading_account.usdc_balance = 1_000_000;
 
         Ok(())
     }
     
-    pub fn trade_in_arena(ctx: Context<TradeInArena>) -> Result<()> {
-        let trading_account_for_arena = &mut ctx.accounts.trading_account_for_arena;
-        let trade_account =  &mut ctx.accounts.trade_account;
+    pub fn open_position(ctx: Context<OpenPosition>, asset: String, quantity: i32) -> Result<()> {
+        require!(asset.len() <= 10, EphemeralRollupError::AssetNameTooLong);
 
-        trade_account.bump = ctx.bumps.trade_account;
-        trade_account.pubkey = *ctx.accounts.signer.key;
-        trading_account_for_arena.trade_count += 1;
+        let trading_account = &mut ctx.accounts.trading_account_for_arena;
+        let open_pos =  &mut ctx.accounts.open_position_account;
+        
+        open_pos.bump = ctx.bumps.open_position_account;
+        trading_account.open_positions_count += 1;
+        
+        let price  =  100; // TODO: get current price of asset using Oracle price feeds.
+
+        require!(quantity * price <= trading_account.usdc_balance, EphemeralRollupError::InsufficientFunds);
+        require!(quantity >= 0, EphemeralRollupError::ShortingUnsupported);
+
+        trading_account.usdc_balance -= quantity * price;
+        open_pos.asset = asset;
+        open_pos.quantity = quantity;
+        
+        Ok(())
+    }
+
+    // this function expects the correct open_position_account already passed. 
+    // Frontend must go through all open position accounts and find the one that belongs to the asset they want to update.
+    pub fn update_position(ctx: Context<UpdateOpenPosition>, quantity: i32) -> Result<()> {
+        let open_pos = &mut ctx.accounts.open_position_account;
+        let trading_account = &mut ctx.accounts.trading_account_for_arena;
+        
+        let price = 100; // TODO: get current price of asset using Oracle price feeds.
+
+        require!(quantity * price <= trading_account.usdc_balance, EphemeralRollupError::InsufficientFunds);
+        require!(open_pos.quantity + quantity >= 0, EphemeralRollupError::ShortingUnsupported);
+        
+        open_pos.quantity += quantity;
+        trading_account.usdc_balance -= quantity * price;
+
+        Ok(())
+    }
+
+    pub fn close_position(ctx: Context<ClosePosition>) -> Result<()> {
+        let open_pos = &ctx.accounts.open_position_account;
+        let trading_account = &mut ctx.accounts.trading_account_for_arena;
+
+        require_keys_eq!(ctx.accounts.signer.key(), trading_account.authority.key(), EphemeralRollupError::Unauthorised);
+
+        let price = 100; // TODO: get current price of asset using Oracle price feeds.
+    
+        trading_account.usdc_balance += open_pos.quantity * price;
+
+        // NOT DOING THIS: refer to TODO/DISCLAIMER on [TradingAccountForArena]
+        // trading_account.open_positions_count -= 1;
+        Ok(())
+    }
+
+    // TODO: for loop that gets all open_position_accounts and closes them
+    pub fn close_all_positions(ctx: Context<OpenPosition>) -> Result<()> {
         Ok(())
     }
 }
@@ -113,9 +166,13 @@ pub struct CreateArena<'info> {
 #[account]
 #[derive(InitSpace)]
 pub struct TradingAccountForArena {
-    pubkey: Pubkey,
+    authority: Pubkey,
     // TODO: figure out the correct datatype for this count. User/agent may trade more than 255 times.
-    trade_count: u8,
+    // TODO/DISCLAIMER: this won't help protect us forever. In case of closing accounts, we might close an account that sits in the middle of the list
+    // find a more scalable way to predictably get accounts.
+    // temp fix: don't subtract from open_positions_count on account close and go until this count on frontend and show data of accounts that are not empty
+    open_positions_count: u8,
+    usdc_balance: i32,
     bump: u8,
 }
 
@@ -138,24 +195,27 @@ pub struct CreateTradingAccountForArena<'info> {
 }
 
 
+
 #[account]
 #[derive(InitSpace)]
-pub struct TradeAccount {
-    // TODO: figure out how to store transactions data in this account
-    pubkey: Pubkey,
-    bump: u8,
+pub struct OpenPositionAccount {
+    // TODO: figure out what length we should put here
+    #[max_len(10)]
+    asset: String,
+    quantity: i32,
+    bump: u8
 }
 
 #[derive(Accounts)]
-pub struct TradeInArena<'info> {
+pub struct OpenPosition<'info> {
     #[account(
         init, 
         payer = signer,
-        space = 8 + TradeAccount::INIT_SPACE,
-        seeds = [b"trade_account", signer.key().as_ref(), trading_account_for_arena.key().as_ref(), &trading_account_for_arena.trade_count.to_le_bytes()],
+        space = 8 + OpenPositionAccount::INIT_SPACE,
+        seeds = [b"open_position_account", signer.key().as_ref(), trading_account_for_arena.key().as_ref(), &trading_account_for_arena.open_positions_count.to_le_bytes()],
         bump
     )]
-    pub trade_account: Account<'info, TradeAccount>,
+    pub open_position_account: Account<'info, OpenPositionAccount>,
 
     #[account(
         mut,
@@ -171,6 +231,42 @@ pub struct TradeInArena<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct UpdateOpenPosition<'info> {
+    #[account(mut)]
+    pub open_position_account: Account<'info, OpenPositionAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"trading_account_for_arena", signer.key().as_ref(), arena_account.key().as_ref()],
+        bump = trading_account_for_arena.bump
+    )]
+    pub trading_account_for_arena: Account<'info, TradingAccountForArena>,
+
+    pub arena_account: Account<'info, ArenaAccount>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ClosePosition<'info> {
+    #[account(mut, close = signer)]
+    pub open_position_account: Account<'info, OpenPositionAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"trading_account_for_arena", signer.key().as_ref(), arena_account.key().as_ref()],
+        bump = trading_account_for_arena.bump
+    )]
+    pub trading_account_for_arena: Account<'info, TradingAccountForArena>,
+
+    pub arena_account: Account<'info, ArenaAccount>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
 
 #[error_code]
 pub enum EphemeralRollupError {
@@ -180,4 +276,10 @@ pub enum EphemeralRollupError {
     UnknownUser,
     #[msg("Name must be 10 characters or smaller")]
     NameTooLong,
+    #[msg("Asset name must be 10 characters or smaller")]
+    AssetNameTooLong,
+    #[msg("Your account does not have enough funds to execute this transactions.")]
+    InsufficientFunds,
+    #[msg("Shorting an asset is not supported as of now.")]
+    ShortingUnsupported,
 }
