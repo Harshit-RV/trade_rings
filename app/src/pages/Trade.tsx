587710a5ae3eb344817406f359c4a6c73883bc5b
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-10-18 16:55:44 +0530

    feat (frontend): implement tabbed navigation for buy/sell actions

--- app/src/pages/Trade.tsx
+++ app/src/pages/Trade.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const Trade = () => {
   return (
@@ -8,6 +10,25 @@ const Trade = () => {
 
         
         <div className="w-[30%] flex-shrink-0 bg-[#303030] h-min py-7 px-7 rounded-4xl gap-5 flex flex-col">
+
+          <Tabs defaultValue="buy" className="w-full">
+            <TabsList className="w-full h-10">
+              <TabsTrigger 
+                className="w-1/2 border-none font-bold hover:cursor-pointer dark:data-[state=active]:bg-[#222D2E] dark:data-[state=active]:text-[#02C178] h-[calc(100%+1px)]" 
+                value="buy"
+              >
+                Buy
+              </TabsTrigger>
+              <TabsTrigger 
+                className="w-1/2 border-none font-bold hover:cursor-pointer dark:data-[state=active]:bg-[#39252A] dark:data-[state=active]:text-[#FA4B4E] h-[calc(100%+1px)]"
+                value="sell"
+              >
+                Sell
+              </TabsTrigger>
+            </TabsList>
+            {/* <TabsContent value="account">Make changes to your account here.</TabsContent>
+            <TabsContent value="password">Change your password here.</TabsContent> */}
+          </Tabs>
           
           <div className="bg-background py-9 px-8 rounded-4xl flex items-center">
             
@@ -25,7 +46,7 @@ const Trade = () => {
             </div>
           </div>
 
-          <div className="bg-background py-9 px-8 rounded-4xl flex items-center">
+          <div className="bg-background py-4 px-8 rounded-4xl flex items-center">
             
             <div className="bg-[#303030] rounded-xl py-2 px-3 flex items-center gap-2 flex-shrink-0">
               <img className="size-5" src="https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png" alt="" />
